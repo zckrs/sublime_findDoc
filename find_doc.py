a70607948a8873898a07cabc6fccf7e8b1df406c
Mehdy Dara <mdara@eleven-labs.com>
2014-02-06 17:54:21 +0100

    Encode the selection for url

--- find_doc.py
+++ find_doc.py
@@ -1,25 +1,32 @@
 # Written by Mehdy Dara (mdara@eleven-labs.com)
 
-import sublime, sublime_plugin, webbrowser
+import sublime, sublime_plugin, webbrowser, urllib
 
 class FindDocSelectionCommand(sublime_plugin.TextCommand):
     def run(self, edit, url):
         text = ""
 
+        # Get the text for each selections and cursors.
         for region in self.view.sel():
-
             if region.empty():
                 word = self.view.word(region)
                 text += self.view.substr(word)
             else:
                 text += self.view.substr(region)
 
+        # Encode the text for url
+        text = urllib.parse.quote(text)
+
+        # Concate url and text
         try:
             url = url % text
         except TypeError:
             url = url
 
+        # Open new tab on url
         if sublime.active_window():
+            # '<b>bold</b>ä'
+            sublime.message_dialog(url)
             sublime.active_window().run_command("prompt_find_doc", {"url": url} )
 
 class PromptFindDocCommand(sublime_plugin.WindowCommand):
